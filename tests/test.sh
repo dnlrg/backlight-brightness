92ec1cf5cb652b6fcbeb3c249f211a60a4caebea
Daniel Rodríguez <6131715+dnlrg@users.noreply.github.com>
2020-10-23 01:24:39 +0200

    test: shellcheck tests

--- tests/test.sh
+++ tests/test.sh
@@ -6,7 +6,7 @@ target_script=$1
 
 clean_up()
 {
-    rm -rf $fake_sysfs
+    rm -rf "$fake_sysfs"
 }
 
 trap clean_up EXIT
@@ -26,38 +26,37 @@ init_backlight()
     path=$1
     type=$2
 
-    mkdir -p $path
+    mkdir -p "$path"
 
     echo 1000 | tee "$path"/brightness > "$path"/max_brightness
-    echo $type > "$path"/type
+    echo "$type" > "$path"/type
 }
 
 fake_sysfs=$(mktemp -d)
 fake_path=$fake_sysfs/fake_backlight
 
-init_backlight $fake_path raw
+init_backlight "$fake_path" raw
 
 export BACKLIGHT_SYSFS=$fake_sysfs
 
-$target_script | grep -qE "^100% \(1000/1000\)$"
+$target_script | grep -qE "^100% \\(1000/1000\\)$"
 
 output=$($target_script 50)
 
 [ -z "$output" ]
 
-$target_script | grep -qE "^50% \(500/1000\)$"
+"$target_script" | grep -qE "^50% \\(500/1000\\)$"
 
 output=$($target_script +10)
 
 [ -z "$output" ]
 
-$target_script | grep -qE "^60% \(600/1000\)$"
+"$target_script" | grep -qE "^60% \\(600/1000\\)$"
 
 output=$($target_script -20)
 
 [ -z "$output" ]
 
-$target_script | grep -qE "^40% \(400/1000\)$"
-
+"$target_script" | grep -qE "^40% \\(400/1000\\)$"
 
 echo OK
